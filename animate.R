877365f6d29900ca88f631467a9096937adcbfdb
adensur <adensur@gmail.com>
2015-06-02 11:05:01 +0400

    Calculated equilibrium states for N=64-68 Calculated density aussicht for N=13, lowest temperature

--- animate.R
+++ animate.R
@@ -1,9 +1,9 @@
-s=0.15
+s=0.02
 
 r=reinit(13)
 r=temp(r,sd=s)
 temperature(r,K=10000)
+#now we need T=0.01
 
-
-df=animate.calc(r,K=100000)
-animate.plot(df,skip=50,xlim=c(-2,2),ylim=c(-2,2))
+df=animate.calc(r,K=1000000)
+animate.plot(df[,,1:500000],skip=50,xlim=c(-2,2),ylim=c(-2,2))
