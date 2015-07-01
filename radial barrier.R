a90eba50fdb30cd7524d9af4c671cc36a0aed034
adensur <adensur@gmail.com>
2015-07-01 20:06:27 +0300

    123

--- radial barrier.R
+++ radial barrier.R
@@ -10,24 +10,26 @@ r=radial_displace(r,dr=0.1,ind=2)
 #
 Us=NULL
 
-drs=seq(0.1,2,by=0.1)
+drs=seq(0.15,1.3,by=0.1)
 r0=reinit(13)
 K=10000
-d=drs[1]
+d=drs[5]
 r=radial_displace(r0,dr = d,ind = 2)
 r2=r
-r2=search2(r2,fixed=c(2),K=K)
-U(r2)
-r2=search2(r2,fixed=c(2),K=K)
-U(r2)
-r2=search2(r2,fixed=c(2),K=K)
-U(r2)
-r2=search2(r2,fixed=c(2),K=K)
-U(r2)
-r2=search2(r2,fixed=c(2),K=K)
-U(r2)
-r2=search2(r2,fixed=c(2),K=K)
-U(r2)
+
+
+for(as in 1:60){
+        r2=search2(r2,fixed=c(2),K=K)
+        U(r2)
+}
+
 Us=c(Us,U(r2))
 dd=c(dd,d)
-qplot(x=dd[-7],y=Us[-7],geom="point")
+qplot(x=dd,y=Us,geom="point")
+
+roes=sapply(dd,function(x) {
+        ro(radial_displace(r0,dr=x,ind=2))[2]
+})
+
+frame=data.frame(rad=roes,U=Us)
+write.csv(frame,"radial_barrier13.csv",row.names=FALSE)
