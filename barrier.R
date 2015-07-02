8115ad2f87b567698b796bd387b2aa26cbfa3e27
adensur <adensur@gmail.com>
2015-07-02 19:32:54 +0300

    123

--- barrier.R
+++ barrier.R
@@ -1,83 +1,26 @@
 source('lib.R')
 
-r=reinit(13)
-r
-
-system.time({
-  r2=search(r,fixed=c(1,2),K=300000)
-})
-print(U(r2))
-
-system.time({
-  Us=NULL
-  dphis=seq(0.01,10,by=0.01)
-  r0=reinit(13)
-  K=100000
-  for(d in dphis){
-    r=rotate(r0,dphi=d,inds=c(2,5,11,12))
-    r2=search(r,fixed=c(1,2),K=K)
-    Us=c(Us,U(r2))
-  }
-})
-qplot(x=dphis,y=Us,geom="line")
-
-
-plot(r0)
-for(d in dphis){
-  r=rotate(r0,dphi=d,inds=c(2,5,11,12))
-  points(r)
-}
-
-
-#sapply
-r0=reinit(13)
-dphis=seq(0,pi,by=0.05)
-K=500000
-system.time({
-        U2=sapply(dphis,function(d) {
-                U(search(rotate(r0,dphi=d,inds=c(2,5,11,12)),fixed=c(1,2),K=K))
-        })
-})
-
-qplot(x=dphis,y=U2,geom="line")
-
-#manual
 Us=NULL
-dphis=seq(0.01,2*pi,by=0.1)
+dphis=seq(0.011,2*pi,by=0.03)
 r0=reinit(14)
+dd=NULL
 
-
-
-K=50000
+K=10000
 d=dphis[4]
-        r=rotate(r0,dphi=d,inds=c(2,5,11,12))
+        r=rotate(r0,dphi=d,inds=c(1,7,9,14))
         r2=r
+
+for(as in 1:60){
         r2=search(r2,fixed=c(1,2),K=K)
-        U(r2)
-        r2=search(r2,fixed=c(1,2),K=K)
-        U(r2)
-        r2=search(r2,fixed=c(1,2),K=K)
-        U(r2)
-        r2=search(r2,fixed=c(1,2),K=K)
-        U(r2)
-        r2=search(r2,fixed=c(1,2),K=K)
-        U(r2)
-        r2=search(r2,fixed=c(1,2),K=K)
-        U(r2)
-        Us=c(Us,U(r2))
+        print(U(r2))
+}
+        
+        
+Us=c(Us,U(r2))
 dd=c(dd,d)
-qplot(x=dd[-7],y=Us[-7],geom="point")
+qplot(x=dd,y=Us,geom="point")
 
 
 frame=data.frame(dphis=dd,Us=Us)
-write.csv(frame,"barrier.csv")
+#write.csv(frame,"barrier14.csv")
 
-d=dphis[52]
-r0=reinit(13)
-K=10000
-
-r=rotate(r0,dphi=d,inds=c(2,5,11,12))
-r2=r
-r2=search(r2,fixed=c(1,2),K=K)
-U(r2)
-U3=U()
--- radial barrier.R
+++ radial barrier.R
@@ -1,26 +1,27 @@
 source('lib.R')
 
-r=reinit(13)
+r=reinit(14)
 r
 plot(r)
-r=radial_displace(r,dr=0.1,ind=2)
+r=radial_displace(r,dr=0.1,ind=1)
 
 
 
 #
 Us=NULL
+dd=NULL
 
 drs=seq(0.15,1.3,by=0.1)
-r0=reinit(13)
+r0=reinit(14)
 K=10000
-d=drs[5]
-r=radial_displace(r0,dr = d,ind = 2)
+d=0
+r=radial_displace(r0,dr = d,ind = 1)
 r2=r
 
 
 for(as in 1:60){
-        r2=search2(r2,fixed=c(2),K=K)
-        U(r2)
+        r2=search2(r2,fixed=c(1),K=K)
+        print(U(r2))
 }
 
 Us=c(Us,U(r2))
@@ -28,8 +29,8 @@ dd=c(dd,d)
 qplot(x=dd,y=Us,geom="point")
 
 roes=sapply(dd,function(x) {
-        ro(radial_displace(r0,dr=x,ind=2))[2]
+        ro(radial_displace(r0,dr=x,ind=1))[1]
 })
 
 frame=data.frame(rad=roes,U=Us)
-write.csv(frame,"radial_barrier13.csv",row.names=FALSE)
+write.csv(frame,"radial_barrier14.csv",row.names=FALSE)
